e199acf8c33c4a8c9c44ddbb242a27180bb9f936
Uweb95 <mail@nbartnick.de>
2025-09-02 12:14:23 +0200

    refactor formatJsonIfPossible to enhance JSON encoding and improve content handling

--- app/helpers.php
+++ app/helpers.php
@@ -1,11 +1,13 @@
 <?php
 
 if (!function_exists('formatJsonIfPossible')) {
-    function formatJsonIfPossible($content) {
+    function formatJsonIfPossible($content): false|string {
+        $content = preg_replace('/\x1b\[[0-9;]*[A-Za-z]/', '', $content);
+        $content = html_entity_decode($content, ENT_QUOTES | ENT_HTML5, 'UTF-8');
         $decoded = json_decode($content, true);
 
         if (json_last_error() === JSON_ERROR_NONE) {
-            return json_encode($decoded, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            return json_encode($decoded, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
         }
 
         return $content;
